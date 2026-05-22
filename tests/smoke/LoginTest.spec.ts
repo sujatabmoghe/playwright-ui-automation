dd473b0513483b9a97204195d031057476985d73
Sujata <sujatabmoghe@gmail.com>
2026-05-22 10:50:58 +0200

    added changed data file

--- tests/smoke/LoginTest.spec.ts
+++ tests/smoke/LoginTest.spec.ts
@@ -4,22 +4,18 @@ import { expect } from '@playwright/test';
 import LoginData from '../../test-data/LoginData.json';
 
 
-test('Successful Login', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoLoginPage();
-    //await loginPage.login('standard_user', 'secret_sauce');
-    await loginPage.login(LoginData['valid_user'].username, LoginData['valid_user'].password);
-    //await loginPage.verifyLoginSuccess();
-     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+LoginData.forEach((data) => {
     
-});
+    if(!data.run) return;
+    test(`Login Test for ${data.username}`, async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.gotoLoginPage();
+        await loginPage.login(data.username, data.password);
 
-test('Unsuccessful Login', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoLoginPage();
-    //await loginPage.login('standard_user', 'secret_sauce');
-    await loginPage.login(LoginData['invalid_user'].username, LoginData['invalid_user'].password);
-    //await loginPage.verifyLoginSuccess();
-     await expect(loginPage.errorMessage).toBeVisible();
-    
+        if(data.expected === 'success'){
+            await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+        } else{
+         await expect(loginPage.errorMessage).toBeVisible();
+        }
+});
 });
